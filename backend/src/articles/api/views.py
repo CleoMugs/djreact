14701be5802e49d6e2c713f6ec573e207c83cf4d
CleoMugs <cleophas.mugeni@gmail.com>
2020-07-14 17:25:55 +0300

    added app

---
+++ backend/src/articles/api/views.py
@@ -0,0 +1,8 @@
+from rest_framework.generics import ListAPIView
+
+from articles.models import Article
+from .serializers import ArticleSerializer
+
+class ArticleListView(ListAPIView):
+	queryset = Article.objects.all()
+	serializer_class = ArticleSerializer
