1bad6c0dd46b1abaa7efc9f3de5eed04b2e35e23
CleoMugs <cleophas.mugeni@gmail.com>
2020-07-17 21:02:44 +0300

    added forms2

--- frontend/gui/src/components/Form.js
+++ frontend/gui/src/components/Form.js
@@ -5,27 +5,35 @@ const FormItem = Form.Item;
 
 
 class CustomForm extends React.Component {
+
+    handleFormSubmit = (event) => {
+      event.preventDefault();
+      const title = event.target.elements.title.value;
+      const content = event.target.elements.content.value;
+
+      console.log(title, content);
+
+    }
   
-  render() {
-    return (
-      <div>
-        <Form>
-    
-          <FormItem  label="Title" >
-            <Input placeholder="Put a title here" />
-          </FormItem>
+    render() {
+      return (
+        <div>
+            <Form onSubmit={this.handleFormSubmit}>
+            <FormItem  label="Title" >
+              <Input name="title" placeholder="Put a title here" />
+            </FormItem>
 
-          <FormItem  label="Content">
-            <Input placeholder="Enter some content ..." />
-          </FormItem>
+            <FormItem  label="Content">
+              <Input name="content" placeholder="Enter some content ..." />
+            </FormItem>
 
-          <FormItem>
-            <Button type="primary">Submit</Button>
-          </FormItem>
-        </Form>
-      </div>
-    );
-  }
+            <FormItem>
+              <Button type="primary" htmlType="submit">Submit</Button>
+            </FormItem>
+            </Form>
+        </div>
+      );
+    }
 }
 
 export default CustomForm;
